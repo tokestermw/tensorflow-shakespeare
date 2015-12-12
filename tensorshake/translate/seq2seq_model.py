8103557cc7204326cf09679a3d865e7042d961f0
tokestermw <tokestermw@gmail.com>
2015-12-11 21:45:16 -0800

    use adam optimizer

--- tensorshake/translate/seq2seq_model.py
+++ tensorshake/translate/seq2seq_model.py
@@ -141,7 +141,8 @@ class Seq2SeqModel(object):
     if not forward_only:
       self.gradient_norms = []
       self.updates = []
-      opt = tf.train.GradientDescentOptimizer(self.learning_rate)
+      # opt = tf.train.GradientDescentOptimizer(self.learning_rate)
+      opt = tf.train.AdamOptimizer(self.learning_rate)
       for b in xrange(len(buckets)):
         gradients = tf.gradients(self.losses[b], params)
         clipped_gradients, norm = tf.clip_by_global_norm(gradients,
