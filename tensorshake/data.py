fede5fa0453f89e0f37a88fa59905726ac7f144a
Motoki Wu <tokestermw@gmail.com>
2017-04-14 09:29:30 -0700

    added batch pad code fixed vocab building (start and end tokens)

--- tensorshake/data.py
+++ tensorshake/data.py
@@ -4,9 +4,11 @@ from __future__ import print_function
 
 import os
 from collections import Counter
+from itertools import izip
 
 from unidecode import unidecode
 from nltk import word_tokenize
+import numpy as np
 
 import utils as shake_utils
 
@@ -51,15 +53,43 @@ def tokenize(text):
     text = unidecode(text)
     text = text.replace("' ", "'")
     tokens = word_tokenize(text)
-    tokens = ["_START"] + tokens + ["_END"]
     return tokens
 
 
 def vectorize(tokens, vocab):
-    return [vocab.get(token, SPECIAL_TOKENS["_OOV"]) for token in tokens]
+    vector = [vocab.get(token, SPECIAL_TOKENS["_OOV"]) for token in tokens]
+    vector = [SPECIAL_TOKENS["_START"]] + vector + [SPECIAL_TOKENS["_END"]]
+    return vector
 
 
-# TODO: cache
+def batch_and_pad(list_of_vectors, maxlen=None):
+    maxlen = max([len(v) for v in list_of_vectors])
+    padded_sequences = np.zeros((len(list_of_vectors), maxlen), dtype=np.int32)
+    for i, v in enumerate(list_of_vectors):
+        l = min(len(v), maxlen)
+        padded_sequences[i, :l] = v[:l]
+    return padded_sequences
+
+
+# TODO: cleaner iterator with batch and pad
+def data_iterator(source_path, source_vocab, target_path, target_vocab, batch_size=32, maxlen=100):
+    batch_source, batch_target, batch_counter = [], [], 0
+
+    for source_text, target_text in izip(_read_data(source_path), _read_data(target_path)):
+        source_tokens = tokenize(source_text)
+        source_vector = vectorize(source_tokens, source_vocab)
+        batch_source.append(source_vector)
+
+        target_tokens = tokenize(target_text)
+        target_vector = vectorize(target_tokens, target_vocab)
+        batch_target.append(target_vector)
+
+        batch_counter += 1
+        if batch_counter >= batch_size:
+            yield batch_and_pad(batch_source, maxlen=maxlen), batch_and_pad(batch_target, maxlen=maxlen)
+            batch_source, batch_target, batch_counter = [], [], 0
+
+
 @shake_utils.cache
 def build_vocab(path, max_vocab=10000, min_counts=5):
     counts = Counter()
@@ -70,6 +100,8 @@ def build_vocab(path, max_vocab=10000, min_counts=5):
     word2idx = {word: idx + len(SPECIAL_TOKENS) for idx, (word, count) in enumerate(counts.most_common(max_vocab)) if count > min_counts}
     word2idx.update(SPECIAL_TOKENS)
     idx2word = [i[0] for i in sorted(word2idx.iteritems(), key=lambda x: x[1])]
+
+    assert max(v for v in word2idx.itervalues()) == (len(word2idx) - 1), "vocab size doesn't match with the indices."
     return word2idx, idx2word
 
 
@@ -83,6 +115,9 @@ def _test():
     text = "how are you?"
     tokens = tokenize(text)
     vector = vectorize(tokens, source_vocab[0])
+
+    for i_, j_ in data_iterator(default_modern_train_path, source_vocab[0], default_modern_train_path, target_vocab[0]):
+        print(i_)
 
 
 if __name__ == "__main__":
