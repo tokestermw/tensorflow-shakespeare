83978c5fe98c8ac3386e50cf089314e25500c618
Motoki Wu <tokestermw@gmail.com>
2017-04-24 14:35:02 -0700

    remove inference.py soon  and put in train.py

--- tensorshake/inference.py
+++ tensorshake/inference.py
@@ -11,8 +11,14 @@ import train as shake_train
 FLAGS = shake_train.FLAGS
 
 
+# TODO: move this to train
 def main(_argv):
-    source_word2idx, source_idx2word = shake_data.build_vocab(FLAGS.source_train)
+    if FLAGS.pretrained_embeddings_path is None:
+        source_word2idx, source_idx2word = shake_data.build_vocab(FLAGS.source_train)
+    else:
+        source_word2idx, source_idx2word, source_embedding_matrix = \
+            shake_data.build_vocab_with_embeddings(FLAGS.pretrained_embeddings_path)
+
     target_word2idx, target_idx2word = shake_data.build_vocab(FLAGS.target_train)
 
     source_vocab_size = len(source_word2idx)
@@ -24,7 +30,12 @@ def main(_argv):
         source_vocab_size,
         num_rnn_layers=FLAGS.num_rnn_layers,
         is_bidi=FLAGS.is_bidi,
-        reverse_sequence=FLAGS.reverse_sequence)
+        reverse_sequence=FLAGS.reverse_sequence,
+        embedding_matrix=source_embedding_matrix,
+        use_batch_norm=FLAGS.use_batch_norm,
+        use_sentence_projection=FLAGS.use_sentence_projection,
+        is_train=False,
+        )
     decoder = shake_model.Decoder(
         target_vocab_size,
         add_attention=FLAGS.add_attention)
